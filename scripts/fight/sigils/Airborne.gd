3eb7cfaed8d8145a80fb79945bd2a06507ecf60b
Mouthless-Stoat <nphuyanh@outlook.com>
2026-07-23 01:49:08 -0300

    simplify airborne condition

--- scripts/fight/sigils/Airborne.gd
+++ scripts/fight/sigils/Airborne.gd
@@ -5,6 +5,6 @@ func replace_action(type: Action.Type, act: Action) -> Array[Action]:
 	if type != Action.Type.CARD_STRIKE:
 		return []
 	var action := act as CardStrikeAction
-	if fight_manager.card_manager.get_card_by_id(action.striker_id) != attached_card:
+	if action.striker_id != attached_card.id:
 		return []
 	return [CardStrikeAction.new(action.pos, action.striker_id, true)]
