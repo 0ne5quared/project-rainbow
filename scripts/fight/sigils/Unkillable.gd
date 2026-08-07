17016191b4bc9dde2a484387ef9aa5ef0c1fe957
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2026-08-07 11:25:18 -0400

    4 sigils
    Frozen Away
    Opossum was spelled wrong
    
    The other 3 are new stuff to make Stoat's work load easier

--- scripts/fight/sigils/Unkillable.gd
+++ scripts/fight/sigils/Unkillable.gd
@@ -4,4 +4,7 @@ extends Sigil
 func on_card_perished(card: Card) -> void:
 	if card != attached_card:
 		return
+	var cd := card.card_data
+	if get_config("nerfed", false) as bool:
+		cd.sigils.remove_at(cd.sigils.find("Unkillable"))
 	add_card(controller_id(), attached_card.id)
