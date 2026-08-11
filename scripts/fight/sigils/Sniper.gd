b5940f759ba2d50dbb4293148165090057037f12
Mouthless-Stoat <nphuyanh@outlook.com>
2026-08-11 00:47:14 -0300

    fix sniper emitting the wrong action

--- scripts/fight/sigils/Sniper.gd
+++ scripts/fight/sigils/Sniper.gd
@@ -2,12 +2,12 @@ extends Sigil
 
 
 func replace_action(type: Action.Type, act: Action) -> Array[Action]:
-	if type != Action.Type.CARD_STRIKE:
+	if type != Action.Type.PRE_CARD_STRIKE:
 		return []
-	var action := act as CardStrikeAction
+	var action := act as PreCardStrikeAction
 	if fight_manager.card_manager.get_card_by_id(action.striker_id) != attached_card:
 		return []
 	var slot := await request_target(
 		controller_id(), func(s: BoardManager.Slot) -> bool: return s.pos.y == BoardManager.Row.OPP
 	)
-	return [CardStrikeAction.new(action.striker_id, slot.pos, action.to_face)]
+	return [PreCardStrikeAction.new(action.striker_id, slot.pos, action.to_face)]
