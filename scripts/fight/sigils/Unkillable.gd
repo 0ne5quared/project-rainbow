e56fe84808b56952c1255b24aa695178817a2bd7
root <root@Junk-PC.localdomain>
2026-09-02 22:36:23 +1000

    Revert "Implemented draw_death common sigil and updated Unkillable to match"
    This reverts commit 4998c6cba48de6f6d8992611b95401e79e6c827a.

--- scripts/fight/sigils/Unkillable.gd
+++ scripts/fight/sigils/Unkillable.gd
@@ -1,8 +1,10 @@
-extends DrawDeathSigil
+extends Sigil
 
 
-func new_form() -> Ruleset.CardData:
-	var cd := attached_card.card_data
+func on_card_perished(card: Card) -> void:
+	if card != attached_card:
+		return
+	var cd := card.card_data
 	if get_config("nerfed", false) as bool:
 		cd.sigils.remove_at(cd.sigils.find("Unkillable"))
-	return cd
+	add_card(controller_id(), attached_card.id)
